74c818e0f8b9ea125769d62138423338b1429a6a
denistanwh <denistanwh@gmail.com>
2015-05-13 13:06:03 -0400

    Update

--- d3/script.js
+++ d3/script.js
@@ -1,4 +1,8 @@
 
+var width = 800,
+	height = 400,
+	margin = 50;
+
 		// extract electricity demand data [MW] for nyc
 		var getDemand = function(data) {
 			var demand = []
@@ -17,9 +21,37 @@
 		}; 
 
 
-		d3.csv("NYC_US_Hourly_Demand.csv")
-		.row(getDate)
-		.get(function(err, data) {draw(data)})
+		d3.csv("NYC_US_Hourly_Demand.csv", function(error, data) {
+			data.forEach(function(d) {
+				d.date = new Date (d.YR, d.M - 1, d.D, d.HR);
+				d.demand = d.DM;
+			}); 
+
+			// scale the range of the data
+			x.domain(d3.extent(data, function(d) {return d.date; }));
+			y.domain([0, d3.max(data, function(d) {return d.demand; })]);
+
+			// add the valueline path
+			svg.append("path")
+				.attr("class", "line")
+				.attr("d", valueline(data)):
+
+			// add the x-axis
+			svg.append("g")
+				.attr(class, "x axis")
+				.attr("transform", "translate(0," + height + ")")
+				.call(xAxis);
+
+			// add the y-axis
+			svg.append("g")
+				.attr("class", "y axis")
+				.call(yAxis)
+
+		});
+
+
+			.get(function(err, data) {draw(data)})
+		}
 
 		var draw = function(data) {
 			d3.select("body").selectAll("div")
@@ -31,5 +63,8 @@
 					var barHeight = d.DM * (1/100);
 					return barHeight + "px"
 				});
+				.style("width", function(d) {
+					var barWidth = width/data.length
+				})
 
 			};
